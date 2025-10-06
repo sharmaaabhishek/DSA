93a70b96a108073500f212a7ec14d17d0c05859b
Abhishek Sharma <119608669+sharmaaabhishek@users.noreply.github.com>
2025-10-06 17:19:28 +0000

    DSA day 1 practice question

---
+++ day1.py
@@ -0,0 +1,26 @@
+# check if a number is in the range of 10 and 25
+
+
+# num = int(input("Enter the number to check if it lies betweenn 10 to 25: "))
+# if num >= 10 and num <=25:
+#     print("Yes it lies!!!!")
+# else:
+#     print("No it doesntttt!")
+
+
+# you got some marks, marks>=90 fall in grade A, marks>=75 fall in grade B , marks <75 falls in grade c, find the grade.
+
+# marks = int(input("Enter the marks you got: "))
+# print("You entered:", marks)
+
+# if marks >= 90:
+#     print("Woooh, You passed with first division with grade 'A'")
+# elif marks >= 75:
+#     print("Good, You passed with second division with grade 'B'")
+# elif marks >= 35:
+#     print("Satisfactory, You passed with third division with grade 'C'")
+# else:
+#     print("You failed, do hard work!!!")
+
+
+# take radius as inuput from user and print area of a circle having that radius
