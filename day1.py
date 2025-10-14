5288f9c516da6f670fb87824ccdd6b40e8c844a8
Abhishek Sharma <abhimp2477@gmail.com>
2025-10-14 23:01:47 +0530

    Update day1.py

--- day1.py
+++ day1.py
@@ -2,7 +2,7 @@
 
 
 # num = int(input("Enter the number to check if it lies betweenn 10 to 25: "))
-# if num >= 10 and num <=25:
+# if 10 <= num <=25:
 #     print("Yes it lies!!!!")
 # else:
 #     print("No it doesntttt!")
@@ -24,3 +24,31 @@
 
 
 # take radius as inuput from user and print area of a circle having that radius
+
+# radius = int(input("Enter the radius of the circle: "))
+# area = 3.14 * radius * radius
+# print("Area of a given circle", area)
+
+# take length of 3 side of triangle from user and check what type of triangle it is
+
+# triangleOne = int(input("Enter first triangle side"))
+# triangleTwo = int(input("Enter second triangle side"))
+# triangleThree = int(input("Enter third triangle side"))
+
+# if triangleOne == triangleTwo == triangleThree:
+#     print("Triangle is Equilateral triangle")
+# elif triangleOne == triangleTwo or triangleOne == triangleThree or triangleTwo == triangleThree:
+#     print("Triangle is Isosceles triangle")
+# else:
+#     print("Triangle is Scalene triangle")
+
+# check if a number is in between 1 and 90 without using any logical operator
+
+# num = int(input("Enter a number: "))
+
+# if num >= 1:
+#     if num <= 90:
+#         print("Number lies between 1 to 90")
+#     else:print("Number is greater than 90")
+# else:
+#     print("Number is not greater than 1")
